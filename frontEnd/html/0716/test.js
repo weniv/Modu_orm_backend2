ed28fd7e6373073ce90074001c4095d60f444d41
nugurejeil <nugurejeil1@gmail.com>
2024-07-17 08:51:25 +0900

    7/16일까지 내용 업로드

---
+++ frontEnd/html/0716/test.js
@@ -0,0 +1,25 @@
+// var val1 = 1;
+// var val1 = 2;
+
+// {
+//     let val2;
+//     const val3;
+// }
+
+// let one = 1;
+// let two = 2;
+// let three = 3;
+
+let one, two, three;
+const arr = [1, 2, 3];
+
+const val4 = arr;
+
+arr.push(5);
+
+function name() {
+
+}
+
+
+// let val2 = 2;
